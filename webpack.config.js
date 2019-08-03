7c5715501f4280c23e1f3d0f407fe7164e52f63e
Simon Nguyen <simonnguyen3054@gmail.com>
2019-08-02 21:34:23 -0700

    add loader to handle css

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], //note: elements are loaded in reversed order. css-loaded(translate css to js) => style-loader(inject style into dom)
+      },
+    ],
+  },
 }
